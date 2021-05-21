fe9f5cc39688671e9eb6d7d7c7f0de62629a0996
Cloud Souvik <67191711+Cloud-Souvik@users.noreply.github.com>
2021-05-22 03:04:02 +0530

    Update Forcesub.py

--- plugins/Forcesub.py
+++ plugins/Forcesub.py
@@ -69,7 +69,6 @@ async def refresh_cb(c, m):
         caption += "\n\n\n**--Uploader Details:--**\n\n"
         caption += f"__📢 Channel Name:__ `{channel.title}`\n\n"
         caption += f"__🗣 User Name:__ @{channel.username}\n\n" if channel.username else ""
-        caption += f"__👤 Channel Id:__ `{channel.id}`\n\n"
         caption += f"__💬 DC ID:__ {channel.dc_id}\n\n" if channel.dc_id else ""
         caption += f"__👁 Members Count:__ {channel.members_count}\n\n" if channel.members_count else ""
 
@@ -78,8 +77,6 @@ async def refresh_cb(c, m):
         caption += "\n\n\n**--Uploader Details:--**\n\n"
         caption += f"__🦚 First Name:__ `{user.first_name}`\n\n"
         caption += f"__🐧 Last Name:__ `{user.last_name}`\n\n" if user.last_name else ""
-        caption += f"__👁 User Name:__ @{user.username}\n\n" if user.username else ""
-        caption += f"__👤 User Id:__ `{user.id}`\n\n"
         caption += f"__💬 DC ID:__ {user.dc_id}\n\n" if user.dc_id else ""
 
     await msg.copy(m.from_user.id, caption=caption)
