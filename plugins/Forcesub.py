44bb265870348a757f010d8dab1c8da7df7f509e
ayusman <67191711+Cloud-Souvik@users.noreply.github.com>
2021-08-08 15:55:36 +0530

    Update Forcesub.py

--- plugins/Forcesub.py
+++ plugins/Forcesub.py
@@ -17,7 +17,7 @@ async def forcesub(c, m):
                await m.reply_text("**Hey you are banned 😜**", quote=True)
                return
         except UserNotParticipant:
-            buttons = [[InlineKeyboardButton(text='Join Update Channel 🔖', url=f"https://t.me/{UPDATE_CHANNEL}")]]
+            buttons = [[InlineKeyboardButton(text='👉 Join Update Channel 👈', url=f"https://t.me/{UPDATE_CHANNEL}")]]
             if m.text:
                 if (len(m.text.split(' ')) > 1) & ('start' in m.text):
                     chat_id, msg_id = m.text.split(' ')[1].split('_')
